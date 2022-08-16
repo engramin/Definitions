ce3eaa7d86e107216c0b66a6dd582eb32ebb5810
engramin <109396225+engramin@users.noreply.github.com>
2022-08-16 16:38:53 +0500

    Add files via upload

---
+++ Definitions.py
@@ -0,0 +1,79 @@
+1#Python
+#is a high level programming language that uses instructions to teach the computer how to perform a task
+
+2#Program
+#is a set of instructions that tells the computer to perform a speciifc task
+
+3#IDE (Integrated Development Environment
+#is a Software application that provides comprehensive facilities to Computer Programmers for Software Development
+
+4#Comments
+#are pieces of code which are ignored by Python Interpreter
+#Anything written after # is considered as Comment
+
+5#String
+#is a sequence of Characters
+#Anything written between the 'Single Quote' or 'Double Quote' is a String in Python
+
+6#Operators
+#are used to perform Operations on Variables & Values
+
+7#Variables
+#act as Containers to store values.These values can be anything,from numbers to text or entire functions.
+
+8#Data Types
+#Variables can store data of different types & different types can do different things.
+#Python has following data types builtin by default
+#Text Type:str
+#Numeric Type:int,float,complex
+#Sequence Type:list,tuple,range
+#Mapping Type:dict
+#Set Type:Set,frozenset
+#Boolean Type:bool
+#Binary Type:bytes,byte array,memoryview
+#None Type:nonetype
+
+9#Comparision & Logical Operators
+#Comparision operators are used to compare two values
+#Logical operators are used to combine conditional statements
+
+10#Type Conversions
+#The process of converting the value of one data type(integer,string,float etc) to another data type is called type conversion.
+#Python has two types of type Conversion
+# Implicit & Explicit Type Conversion
+
+#Implicit Type Conversion
+#Python automatically convert one data type to another data type.This process doesn't need any user involvement.
+
+#Explicit Type Conversion
+#users convert the data type of an object to required data type.
+
+11#If,Else,Elif Statements
+#Decision making is required when we want to execute a code only if a certain condition is certified
+#The if,elif,else statement is used in Python for decision making
+
+12#Functions
+#is a block of code which only runs when it is called
+#You can pass data,Known as parameters,into a function.
+#A function can return data as a result
+#In python function is defined using the def Keyword
+
+13#Loops
+#pyhton has two Primitive Loop Commands
+#While & For Loop
+
+#While Loop
+#With the while loop we can execute as a set of statements as long as a condition is true.
+#With the break statement we can stop the loop even if the while condition is true
+#With the continue statement we can stop the current iteration,and continue with the next
+#With the else statement we can run a block code once when the condition no longer is true
+
+#For Loop
+#A for loop is used for iterating over a sequence.
+#With the break statement we can stop the loop before it has looped through all the items
+#With the continue statement we can stop the current iteration from the loop,and continue with the next
+
+14#Libraries
+#Python has a built in module that you can use for mathematical tasks
+#Python has a built in module that you can use to calculate mathematical statistics of numerica data
+
